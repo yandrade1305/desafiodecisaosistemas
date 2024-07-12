8e9562e2853bd7ee7bfc9b6b4359fa2459a0c775
yandrade1305 <yan13052001@gmail.com>
2024-07-12 18:54:18 -0300

    Subindo parte inicial do frontend

---
+++ frontend/src/app/todolist/todolist.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent implements OnInit {
+  taskArray = [{ taskName: 'Brush teeth', isCompleted: false, isEditable: false }];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false,
+      isEditable: false
+    })
+
+    form.reset();
+  }
+
+  onDelete(index: number) {
+    console.log(index);
+
+    this.taskArray.splice(index, 1);
+  }
+
+  onCheck(index: number) {
+    console.log(this.taskArray);
+
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
+
+  onEdit(index: number) {
+    this.taskArray[index].isEditable = true;
+  }
+
+  onSave(index: number, newtask: string) {
+    this.taskArray[index].taskName = newtask;
+    this.taskArray[index].isEditable = false;
+  }
+
+
+
+}
